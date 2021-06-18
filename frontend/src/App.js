f813488a32e6009123e1517dccb93ffe2b1aee3c
bloier <airinaight@gmail.com>
2021-06-18 17:27:03 +0200

    Add prettify to scripts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 export default function App() {
-
   return (
-      <div>
-        Hello World
-      </div>
+    <Router>
+      <Switch>
+        <Route path={["/", "/home"]}>
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
-
